98abbab88428e4ec1ee377cc1c3e984d9d09a49e
Katharine Cooney <katharinecooney@gmail.com>
2019-05-26 14:10:08 +0200

    Add basic style to ProfileGroups page

--- src/pages/ProfileGroups.js
+++ src/pages/ProfileGroups.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import profile from '../lib/profile-service';
 import {Link} from 'react-router-dom';
+import '../stylesheets/profileGroups.css';
+import Navbar from '../components/Navbar';
+
 
 class ProfileGroups extends Component {
 
@@ -23,21 +26,34 @@ class ProfileGroups extends Component {
     const {loading, groups} = this.state;
     console.log(groups);
     return (
-      <div>
-      {loading ? <p>Loading...</p> : (
-        groups.map(({group}) => {
-          return(
-            <div key={group._id}>
-            <Link to={'/group/' + group._id}>
-              <p>{group.school}</p>
-              <p>{group.subject}</p>
-            </Link>
+      <div className="profile-groups-page">
+        <div className="profile-groups-banner">
+          <h1>View Your Groups</h1>
+        </div>
 
-              
-            </div>)})
-      )}
-      <Link to="/group/add-group">Create Group</Link>
-      <Link to="/group/join-group">Join Group</Link>
+        <div className="profile-groups-container">
+          {loading ? <p>Loading...</p> : (
+            groups.map(({group}) => {
+              return(
+                <div className="profile-group">
+                  <div key={group._id}>
+                    <Link to={'/group/' + group._id}>
+                      <p>{group.school}</p>
+                      <p>{group.subject}</p>
+                    </Link>
+                  </div>
+                </div>
+                )})
+              )}
+        </div>
+
+        <div className="profile-groups-button-container">
+          <Link to="/group/add-group">Create Group</Link>
+          <Link to="/group/join-group">Join Group</Link>
+        </div>
+        
+        <Navbar />
+
       </div>
     );
   }
