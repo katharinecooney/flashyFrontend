f76c368c7878e7224d53abcd5cbc45f144907e8f
Katharine Cooney <katharinecooney@gmail.com>
2019-05-28 12:36:32 +0200

    Made further progress flipping cards on SingleGroup

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -1,34 +1,34 @@
-import axios from "axios";
+import axios from 'axios';
 
 class Auth {
-  constructor() {
+  constructor () {
     this.auth = axios.create({
-      baseURL: "http://localhost:5000",
+      baseURL: process.env.REACT_APP_API_URL,
       withCredentials: true
     });
   }
 
-  signup(user) {
+  signup (user) {
     const { username, password, firstName, lastName, email } = user;
-    console.log(user)
+    console.log(user);
     return this.auth
-      .post("/auth/signup", { username, password, firstName, lastName, email  })
+      .post('/auth/signup', { username, password, firstName, lastName, email })
       .then(({ data }) => data);
   }
 
-  login(user) {
+  login (user) {
     const { username, password } = user;
     return this.auth
-      .post("/auth/login", { username, password })
+      .post('/auth/login', { username, password })
       .then(({ data }) => data);
   }
 
-  logout() {
-    return this.auth.post("/auth/logout", {}).then(response => response.data);
+  logout () {
+    return this.auth.post('/auth/logout', {}).then(response => response.data);
   }
 
-  me() {
-    return this.auth.get("/auth/me").then(response => response.data);
+  me () {
+    return this.auth.get('/auth/me').then(response => response.data);
   }
 }
 
--- src/pages/SingleGroup.js
+++ src/pages/SingleGroup.js
@@ -39,76 +39,75 @@ class SingleGroup extends Component {
     )
   }
 
-  render(){
-    return(
+  // render(){
+  //   return(
       
-     <div className={this.state.isFlipped ? "flip-card card-flip-motion" : "flip-card"}>
-      <div className={this.state.isFlipped ? "flip-card-inner card-flip-motion" : "flip-card-inner"}>
-        <div className="flip-card-front">
-          <h1>hi</h1>
-          <button onClick={this.handleFlip}>flip</button>
-        </div>
-        <div className="flip-card-back">
-          <h1>bye</h1>
-          <button onClick={this.handleFlip}>flip</button>
-        </div>
-      </div>
-    </div>
-    )
-  }
-
- 
-
-  // render () {
-  //   const {school, subject, flashcards, groupId, isFlipped} = this.state;
-  //   const groupDeckCards = flashcards.length ? (
-  //     flashcards.map(card => {
-  //       return (
-  //         <div className="group-flashcard-flipper" key={card._id}>
-  //           <div className="group-flashcard">
-  //             <div className={this.state.isFlipped ? "hidden" : "group-flashcard-front"}>
-  //               <h3>QUESTION</h3>
-  //               <p>{card.frontText}</p>
-  //               <button onClick={(event) => {
-  //                 this.handleSave(event, card._id)
-  //                 }}>SAVE</button>
-  //               <button onClick={this.handleFlip}>FLIP</button>
-  //             </div>
-
-  //             <div className={this.state.isFlipped ? "hidden" : "group-flashcard-back"}>
-  //               <h3>ANSWER</h3>
-  //               <p>{card.backText}</p>
-  //               <button onClick={(event) => {
-  //                 this.handleSave(event, card._id)
-  //                 }}>SAVE</button>
-  //               <button onClick={this.handleFlip}>FLIP</button>
-  //             </div>
-  //           </div>
-  //         </div>
-  //         )
-  //       })
-  //   ) 
-  //   : 
-  //   (<div>No flashcards saved</div>)
-    
-  //   return (
-  //     <div className="single-group-page">
-  //       <div className="single-group-banner">
-  //         <h1>{school}</h1>
-  //         <h3>{subject}</h3>
-  //         <Link to={{
-  //           pathname: '/group/' + groupId + '/new-card'
-  //         }}>
-  //         <span>Add New Card</span>
-  //         </Link>
+  //    <div className={this"flip-card"}>
+  //     <div className="flip-card-inner">
+  //       <div className="flip-card-front">
+  //         <h1>hi</h1>
+  //         <button onClick={this.handleFlip}>flip</button>
   //       </div>
-  //       <div className="group-flashcard-container">
-  //         <div>{groupDeckCards}</div>
+  //       <div className="flip-card-back">
+  //         <h1>bye</h1>
+  //         <button onClick={this.handleFlip}>flip</button>
   //       </div>
-  //       <Navbar />
   //     </div>
-  //   );
+  //   </div>
+  //   )
   // }
+
+  render () {
+      const {school, subject, flashcards, groupId, isFlipped} = this.state;
+      const groupDeckCards = flashcards.length ? (
+        flashcards.map(card => {
+          return (
+            <div className="group-flashcard-container" key={card._id}>
+              <div className={this.state.isFlipped ? "card-flip-motion group-flashcard" : "group-flashcard"}>
+                
+                <div className="group-flashcard-front">
+                  <h3>QUESTION</h3>
+                  <p>{card.frontText}</p>
+                  <button onClick={(event) => {
+                    this.handleSave(event, card._id)
+                    }}>SAVE</button>
+                  <button onClick={this.handleFlip}>FLIP</button>
+                </div>
+  
+                <div className="group-flashcard-back">
+                  <h3>ANSWER</h3>
+                  <p>{card.backText}</p>
+                  <button onClick={(event) => {
+                    this.handleSave(event, card._id)
+                    }}>SAVE</button>
+                  <button onClick={this.handleFlip}>FLIP</button>
+                </div>
+              </div>
+            </div>
+            )
+          })
+      ) 
+      : 
+      (<div>No flashcards saved</div>)
+      
+      return (
+        <div className="single-group-page">
+          <div className="single-group-banner">
+            <h1>{school}</h1>
+            <h3>{subject}</h3>
+            <Link to={{
+              pathname: '/group/' + groupId + '/new-card'
+            }}>
+            <span>Add New Card</span>
+            </Link>
+          </div>
+          <div className={this.state.isFlipped ? "group-container card-flip-motion" : "group-container"}>
+            <div>{groupDeckCards}</div>
+          </div>
+          <Navbar />
+        </div>
+      );
+    }
 }
 
 export default SingleGroup;
